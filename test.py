1471ebbb70dda35f6251ceedf017b9d0c34d2802
ChanZiXuan <167169327+ChanZiXuan@users.noreply.github.com>
2024-09-05 17:53:47 +0800

    Update test.py

--- test.py
+++ test.py
@@ -4,9 +4,10 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
+import joblib
 
 # Load your logistic regression model
-lr_model = load('heartdisease_logisticregression.joblib')
+lr_model = joblib.load('heartdisease_logisticregression.joblib')
 
 # Streamlit application starts here
 def main():
