5a03b269bd0197648ea86062042c456f9f90634c
ChanZiXuan <167169327+ChanZiXuan@users.noreply.github.com>
2024-09-05 18:50:04 +0800

    Update test.py

--- test.py
+++ test.py
@@ -37,21 +37,21 @@ exercise_angina = 1 if exercise_angina == "Yes" else 0
 st_slope_mapping = {"Up": 0, "Flat": 1, "Down": 2}
 st_slope = st_slope_mapping[st_slope]
 
-input_data = np.array([[age, sex, chest_pain_type, resting_bp, cholesterol, fasting_bs, resting_ecg, max_hr, exercise_angina, oldpeak, st_slope]])
+# input_data = np.array([[age, sex, chest_pain_type, resting_bp, cholesterol, fasting_bs, resting_ecg, max_hr, exercise_angina, oldpeak, st_slope]])
 
-# input_data = pd.DataFrame({
-#     'Age': [age],
-#     'Sex': [sex],
-#     'ChestPainType': [chest_pain_type],
-#     'RestingBP': [resting_bp],
-#     'Cholesterol': [cholesterol],
-#     'FastingBS': [fasting_bs],
-#     'RestingECG': [resting_ecg],
-#     'MaxHR': [max_hr],
-#     'ExerciseAngina': [exercise_angina],
-#     'Oldpeak': [oldpeak],
-#     'ST_Slope': [st_slope]
-# })
+input_data = pd.DataFrame({
+    'Age': [age],
+    'Sex': [sex],
+    'ChestPainType': [chest_pain_type],
+    'RestingBP': [resting_bp],
+    'Cholesterol': [cholesterol],
+    'FastingBS': [fasting_bs],
+    'RestingECG': [resting_ecg],
+    'MaxHR': [max_hr],
+    'ExerciseAngina': [exercise_angina],
+    'Oldpeak': [oldpeak],
+    'ST_Slope': [st_slope]
+})
 
 if st.button("Predict Heart Disease"):
     prediction = lr_model.predict(input_data)
